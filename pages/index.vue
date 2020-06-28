f14d48560406402a6f2c377d382011426c01cdfc
nakajima1412 <nakajima1412@icloud.com>
2020-06-29 00:36:15 +0900

    axiosEvent追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
     <h1>Events</h1>
-    <input v-model="username" type="text" />
-    <nuxt-link :to="{ name: 'username', params: { username } }">
-      user page
-    </nuxt-link>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue'
+
 export default {
+  components: {
+    EventCard,
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
+        return {
+          events: response.data,
+        }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time, please try again',
+        })
+      })
+  },
   data() {
     return {
       username: 'nanashi',
