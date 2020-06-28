6b8ad6fc66e394b97589341ec6e842317fbd336c
nakajima1412 <nakajima1412@icloud.com>
2020-06-28 13:25:13 +0900

    nuxtの設定変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,5 +63,17 @@ export default {
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
    */
-  build: {},
+  build: {
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue|ts)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+        })
+      }
+    },
+  },
 }
